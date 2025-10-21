d54d1592fd420f6f56457a7957559bce8ad88455
satorish <satorizoom51@gmail.com>
2025-10-22 01:06:58 +0200

    add chans

--- Practice/port-scanner/port-scanner.go
+++ Practice/port-scanner/port-scanner.go
@@ -3,41 +3,83 @@ package main
 import (
 	"fmt"
 	"net"
-	"sync" // для синхронизированного сканирования, потому что Go слишком быстрый
+	//"sync" // для синхронизированного сканирования, потому что Go слишком быстрый
 )
 
-var wg sync.WaitGroup
+// var wg sync.WaitGroup
 var openPorts []int
 
 func main() {
-	for i := 1; i < 1024; i++ {
-		wg.Add(1) // зарегистрировать новую горутину
-		go scanPort(i)
+	ports := make(chan int, 100)
+	results := make(chan int)
+
+	for i := 0; i < cap(ports); i++ {
+		go worker(ports, results)
 	}
 
-	wg.Wait() // ждать конца выполнения горутин
+	go func() {
+		for i := 1; i <= 1024; i++ {
+			ports <- i
+		}
+	}()
 
-	sortSlace(&openPorts)
-	for _, v := range openPorts {
-		fmt.Println("OPEN\t", v)
+	for i := 0; i < 1024; i++ {
+		port := <-results
+		if port != 0 {
+			openPorts = append(openPorts, port)
+		}
+	}
+
+	/*
+		wg.Wait() // ждать конца выполнения горутин
+
+		if len(openPorts) > 0 {
+			sortSlace(&openPorts)
+			for _, v := range openPorts {
+				fmt.Println("OPEN\t", v)
+			}
+		}
+	*/
+
+	close(ports)
+	close(results)
+
+	sortSlice(&openPorts)
+	for _, port := range openPorts {
+		fmt.Println(port, "\topen")
 	}
 }
 
-func scanPort(index int) {
-	defer wg.Done() // горутина завершила работу; DEFER - команда выполнится при ВЫХОДЕ из функции
+func worker(ports, results chan int) {
+	for p := range ports { // выполняется пока канал открыт
+		address := fmt.Sprintf("scanme.nmap.org:%d", p)
+		conn, err := net.Dial("tcp", address)
+		if err != nil {
+			results <- 0
+			continue
+		}
+		conn.Close()
 
-	var address string = fmt.Sprintf("scanme.nmap.org:%d", index)
-
-	conn, err := net.Dial("tcp", address)
-	if err != nil {
-		return
+		results <- p
 	}
-	conn.Close()
-
-	openPorts = append(openPorts, index)
 }
 
-func sortSlace(slice *[]int) {
+/*
+	func scanPortWithWG(index int) {
+		defer wg.Done() // горутина завершила работу; DEFER - команда выполнится при ВЫХОДЕ из функции
+
+		var address string = fmt.Sprintf("scanme.nmap.org:%d", index)
+
+		conn, err := net.Dial("tcp", address)
+		if err != nil {
+			return
+		}
+		conn.Close()
+
+		openPorts = append(openPorts, index)
+	}
+*/
+func sortSlice(slice *[]int) {
 	var value2 int
 
 	for i := 0; i < len(*slice)-1; i++ {
