50b5d98df272ba446687b79138c5f17d6099abfb
satorish <satorizoom51@gmail.com>
2025-10-25 13:05:02 +0200

    add echo-servers

--- Practice/reader&writer/reader&writer.go
+++ Practice/reader&writer/reader&writer.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"os"
 )
@@ -10,7 +11,7 @@ type FooReader struct{}
 
 // FooReader полностью соответствует контракту io.Reader
 // Значит, FooReader теперь тоже является io.Reader-ом
-func (FooReader *FooReader) Read(b []byte) (int, error) {
+func (FooReader *FooReader) Read(b []byte) (int, error) { // ПАТТЕРН Decorator, оборачиваем этот интерфейс, чтобы добавить свои действия, например логирование
 	fmt.Println("in > ")
 	return os.Stdin.Read(b)
 }
@@ -26,20 +27,28 @@ func main() {
 	var reader FooReader
 	var writer FooWriter
 
-	input := make([]byte, 4096)
+	/*
+		input := make([]byte, 4096)
 
-	s, err := reader.Read(input)
-	if err != nil {
-		log.Fatalln("Unable to read data")
+		s, err := reader.Read(input)
+		if err != nil {
+			log.Fatalln("Unable to read data")
+		}
+
+		fmt.Printf("Read %d bytes from stdin\n", s)
+
+		// допустим прочитали hi, это будет [h,i,\n] (3 байта)
+		s, err = writer.Write(input[:s]) // если s = 3, выведутся 0, 1 и 2
+		if err != nil {
+			log.Fatalln("Unable to write data")
+		}
+
+		fmt.Printf("Wrote %d bytes to stdout\n", s)
+	*/
+
+	// ЛИБО:
+
+	if _, err := io.Copy(&writer, &reader); err != nil {
+		log.Fatalln("Unable to read/write data")
 	}
-
-	fmt.Printf("Read %d bytes from stdin\n", s)
-
-	// допустим прочитали hi, это будет [h,i,\n] (3 байта)
-	s, err = writer.Write(input[:s]) // если s = 3, выведутся 0, 1 и 2
-	if err != nil {
-		log.Fatalln("Unable to write data")
-	}
-
-	fmt.Printf("Wrote %d bytes to stdout\n", s)
 }
---
+++ WEB/echo-server/echo-server-ddos/go.mod
@@ -0,0 +1,3 @@
+module echo-server-ddos.go
+
+go 1.25.3
