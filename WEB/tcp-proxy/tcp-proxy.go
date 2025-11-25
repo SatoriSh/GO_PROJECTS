814c191a10835cb52019acfd90c089e6dcadfc01
satorish <satorizoom51@gmail.com>
2025-11-25 17:42:00 +0100

    update tcp-proxy

--- WEB/tcp-proxy/tcp-proxy.go
+++ WEB/tcp-proxy/tcp-proxy.go
@@ -9,32 +9,32 @@ import (
 func handle(src net.Conn) {
 	dst, err := net.Dial("tcp", "example.com:80")
 	if err != nil {
-		log.Fatalln("Unable to connect to our unreachable host")
+		log.Fatalln("Failed to connect to destination")
 	}
 	defer dst.Close()
 
 	go func() {
-		if _, err := io.Copy(dst, src); err != nil { // io.Copy(куда, откуда)
-			log.Fatalln(err)
+		if _, err := io.Copy(dst, src); err != nil {
+			log.Fatalln("Forward error:", err)
 		}
 	}()
 
 	if _, err := io.Copy(src, dst); err != nil {
-		log.Fatalln(err)
+		log.Fatalln("Response error:", err)
 	}
 }
 
 func main() {
 	listener, err := net.Listen("tcp", ":8080")
 	if err != nil {
-		log.Fatalln("Unable to bind to port")
+		log.Fatalln("Listen failed")
 	}
 
 	log.Println("Listening on 0.0.0.0:8080")
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
-			log.Fatalln("Unable to accept connection")
+			log.Fatalln("Accept failed:", err)
 		}
 
 		go handle(conn)
