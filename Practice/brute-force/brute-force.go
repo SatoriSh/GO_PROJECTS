b623aa79076a5b113314c1c4276d07b607490df0
satorish <satorizoom51@gmail.com>
2026-01-04 13:50:06 +0100

    upd brute-force.go

--- Practice/brute-force/brute-force.go
+++ Practice/brute-force/brute-force.go
@@ -8,13 +8,14 @@ import (
 	"strings"
 )
 
-var passwdHash string
-var candidateHash string
+var passwdHash string    // хеш который мы ищем
+var candidateHash string // хеш текущего кандидата
 var charset = []byte("0123456789!$-_@#abcdefghijklmnopqrstuvwxyz")
-var stop bool = false
+var stop bool = false // флаг остановки
 var reader = bufio.NewReader(os.Stdin)
 
 func main() {
+	// запрашиваем хеш у пользователя
 	fmt.Println("\nEnter your hash:")
 	fmt.Print("/> ")
 	input, err := reader.ReadString('\n')
@@ -23,8 +24,7 @@ func main() {
 	}
 	passwdHash = strings.TrimSpace(input)
 
-	fmt.Println()
-
+	// перебор паролей разной длины
 	maxPasswdLength := 20
 	for len := 1; len <= maxPasswdLength; len++ {
 		candidate := make([]byte, len)
@@ -38,14 +38,18 @@ func main() {
 func brute(candidate []byte, index int, passwdLength int) {
 	for _, ch := range charset {
 		candidate[index] = ch
-		//fmt.Print("\r ", string(candidate)) // TOO LONG!!!
 
+		//fmt.Print(" ", string(candidate)) // ОЧЕНЬ ДОЛГО!!!
+
+		// рекурсивный вызов для следующей позиции
 		if index < passwdLength-1 {
 			brute(candidate, index+1, passwdLength)
 		}
 		if stop {
 			break
 		}
+
+		// проверка найденного пароля
 		if hashEqual(candidate) {
 			fmt.Println("\n\n", "hash", string(candidate), "=", passwdHash)
 			fmt.Println("\n\n DONE! Here is your password:", string(candidate))
@@ -56,8 +60,8 @@ func brute(candidate []byte, index int, passwdLength int) {
 }
 
 func hashEqual(candidate []byte) bool {
-	sum := md5.Sum(candidate)
-	candidateHash = fmt.Sprintf("%x", sum)
+	sum := md5.Sum(candidate)              // вычисляем MD5
+	candidateHash = fmt.Sprintf("%x", sum) // преобразуем в строку
 
-	return candidateHash == passwdHash
+	return candidateHash == passwdHash // сравниваем
 }
